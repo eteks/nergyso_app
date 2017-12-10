b6bd64b9cc818c05689341ddcee86932325228a9
Ravichandran <ravi@etekchnoservices.com>
2017-12-10 18:06:15 +0530

    news full view page started

--- EnergySoft/app/src/main/java/com/example/user/energysoft/MainActivity.java
+++ EnergySoft/app/src/main/java/com/example/user/energysoft/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.user.energysoft;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,8 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(0xFFFFFFFF);
 
+
+
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            int ONE_TIME = 0;
+
+            @Override
+            public void onClick(View view) {
+                Log.i("clicks","You Clicked B1");
+                Intent i=new Intent(MainActivity.this, FullNews.class);
+                startActivity(i);
+                ONE_TIME++;
+                if (ONE_TIME == 1) {
+                    EditText username = (EditText) findViewById(R.id.username);
+                    EditText password = (EditText) findViewById(R.id.password);
+                    String userName = username.getText().toString();
+                    String passWord = password.getText().toString();
+                    System.out.println(userName+passWord);
+                }
+            }
+        });
 
     }
 
