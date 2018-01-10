fa045f7937dca2bd37d6e47e314e12815c587b2e
Ravichandran <ravi@etekchnoservices.com>
2018-01-10 17:53:42 +0530

    package name changed

---
+++ EnergySoft/app/src/main/java/com/vdart/example/app/quiz_activity_frag.java
@@ -0,0 +1,13 @@
+package com.vdart.example.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quiz_activity_frag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_frag);
+    }
+}
