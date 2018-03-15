2ad770226c95c4ca0bf59d39b7b2e5b9ec4fe079
Ravichandran <ravi@etekchnoservices.com>
2018-03-15 16:37:41 +0530

    search pages and search result functions

--- EnergySoft/app/src/main/java/com/vdart/apps/app/SearchResults.java
+++ EnergySoft/app/src/main/java/com/vdart/apps/app/SearchResults.java
@@ -26,6 +26,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.vdart.apps.app.utils.PaginationScrollListener;
@@ -246,6 +247,111 @@ public class SearchResults extends AppCompatActivity implements Download_data.do
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        MenuItem menuItem = item;
+        Intent intent;
+        switch (item.getItemId())
+        {
+            case R.id.action_home:
+                intent = new Intent(this,BannerActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.profile:
+                intent = new Intent(this,ProfileActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.feedback:
+                intent = new Intent(this,Feedback.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_search:
+                intent = new Intent(this,SearchActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.events:
+                intent = new Intent(this,EventMain.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.news:
+                intent = new Intent(this,NewsMain.class);
+                startActivity(intent);
+                return true;
+
+//            case R.id.shoutout:
+//                intent = new Intent(this,ListingMore.class);
+//                startActivity(intent);
+//                return true;
+
+            case R.id.gallery:
+                intent = new Intent(this,EventGallery.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.info:
+                Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.settings:
+                intent = new Intent(this,Changepassword_Activity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.logout:
+                intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.facebook:
+                intent = new Intent(this,FacebookActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.twitter:
+                intent = new Intent(this,TwitterActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_notification:
+                intent = new Intent(this,NotificationMain.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.ceomsg:
+                intent = new Intent(this,CeomessageActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.live:
+                intent = new Intent(this,LiveTelecast.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.polls:
+                intent = new Intent(this,quiz_activity_frag.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_refresh:
+//                intent = new Intent(this,BannerActivity.class);
+                startActivity(getIntent());
+                return true;
+
+            case R.id.shoutout:
+                intent = new Intent(this,ListingMore.class);
+                intent.putExtra("more","shoutout");
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
